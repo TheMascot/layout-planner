b19986e7281bd08fa04dbf55f7a6968e8469a8fd
Attila Fekete <feketeattila777@proton.me>
2026-08-05 17:27:23 +0200

    Stop tracking IDE/build folders

---
+++ backend/src/test/java/com/mascot/layout_planner/LayoutPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mascot.layout_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
